9ad970f60e49acf3b7bed0cec752b73c9f2246cc
lipd <lipeidao@gmail.com>
2021-06-05 23:20:29 +0800

    feat: 配置 react-router 和 webpack

--- scripts/config/webpack.development.js
+++ scripts/config/webpack.development.js
@@ -15,6 +15,7 @@ module.exports = merge(common, {
     compress: true, // 是否启用 gzip 压缩
     open: true, // 打开默认浏览器
     hot: true, // 热更新
+    historyApiFallback: true,
     proxy: {
       ...proxySetting,
     },
